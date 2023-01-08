4e94d2905e483bb9dacc08b099750e335c368031
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2023-01-07 19:49:07 -0500

    Eslint and Prettier config added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
+import { Inter } from '@next/font/google';
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from '@next/font/google';
+
 import styles from '../styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+const Home = () => {
 	return (
 		<>
 			<Head>
@@ -14,12 +15,14 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+
 			<main className={styles.main}>
 				<div className={styles.description}>
 					<p>
 						Get started by editing&nbsp;
 						<code className={styles.code}>pages/index.tsx</code>
 					</p>
+
 					<div>
 						<a
 							href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -48,6 +51,7 @@ export default function Home() {
 						height={37}
 						priority
 					/>
+
 					<div className={styles.thirteen}>
 						<Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
 					</div>
@@ -63,6 +67,7 @@ export default function Home() {
 						<h2 className={inter.className}>
 							Docs <span>-&gt;</span>
 						</h2>
+
 						<p className={inter.className}>
 							Find in-depth information about Next.js features and&nbsp;API.
 						</p>
@@ -77,6 +82,7 @@ export default function Home() {
 						<h2 className={inter.className}>
 							Learn <span>-&gt;</span>
 						</h2>
+
 						<p className={inter.className}>
 							Learn about Next.js in an interactive course with&nbsp;quizzes!
 						</p>
@@ -91,6 +97,7 @@ export default function Home() {
 						<h2 className={inter.className}>
 							Templates <span>-&gt;</span>
 						</h2>
+
 						<p className={inter.className}>
 							Discover and deploy boilerplate example Next.js&nbsp;projects.
 						</p>
@@ -105,6 +112,7 @@ export default function Home() {
 						<h2 className={inter.className}>
 							Deploy <span>-&gt;</span>
 						</h2>
+
 						<p className={inter.className}>
 							Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.
 						</p>
@@ -113,5 +121,6 @@ export default function Home() {
 			</main>
 		</>
 	);
-}
+};
 
+export default Home;
